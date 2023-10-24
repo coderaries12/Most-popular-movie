d1bfa8871402ad65e31b784e673557afd51abd2c
Sheena Gupta <aries.sheen@gmail.com>
2023-10-24 17:30:54 +0000

    addUser method added

--- code.js
+++ code.js
@@ -3,5 +3,9 @@ class MostPopularMovieSystem {
     constructor() {
       this.userMovies = {};  //Initialize an empty object that will store the movie preferences of each user
     }
+
+    addUser(userId, movies) {
+      this.userMovies[userId] = new Set(movies);  //This method allows you to add a user and their list of favorite movies to the userMovies object. It takes userId and an array of movies.
+    }
   
 }
