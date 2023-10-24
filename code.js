adf4e794719e83cc5b873768b8f54b0446f4b687
Sheena Gupta <aries.sheen@gmail.com>
2023-10-24 17:57:05 +0000

    added getFriends method

--- code.js
+++ code.js
@@ -8,39 +8,21 @@ class MostPopularMovieSystem {
       this.userMovies[userId] = new Set(movies);  //This method allows you to add a user and their list of favorite movies to the userMovies object. It takes userId and an array of movies.
     }
 
-    getMostPopularMovie(userId) {
-      if (!this.userMovies[userId]) {
-        return null;
-      }
+    
+    
+
+    getFriends(userId) {
+      // You can implement a method to fetch a user's friends here.
+      // This example assumes a simple list of friends.
+      const friends = [
+        { id: 1, friends: [2, 3, 4] },
+        { id: 2, friends: [1, 5, 6] },
+        { id: 3, friends: [1, 7, 8] },
+        { id: 4, friends: [1, 9, 10] },
+      ];
   
-      const userMovies = this.userMovies[userId];
-      const friendMovies = new Set();
-  
-      this.getFriends(userId).forEach((friend) => {
-        friendMovies.add(...(this.userMovies[friend] || []));
-      });
-  
-      const recommendedMovies = [...friendMovies].filter((movie) => !userMovies.has(movie));
-  
-      if (recommendedMovies.length === 0) {
-        return null;
-      }
-  
-      const movieCounts = {};
-  
-      this.getFriends(userId).forEach((friend) => {
-        recommendedMovies.forEach((movie) => {
-          if (this.userMovies[friend] && this.userMovies[friend].has(movie)) {
-            movieCounts[movie] = (movieCounts[movie] || 0) + 1;
-          }
-        });
-      });
-  
-      const mostPopularMovie = Object.keys(movieCounts).reduce((a, b) =>
-        movieCounts[a] > movieCounts[b] ? a : b
-      );
-  
-      return mostPopularMovie;
+      const user = friends.find((friend) => friend.id === userId);
+      return user ? user.friends : [];
     }
   
   
