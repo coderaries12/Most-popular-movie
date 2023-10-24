96fb227ca53cb21fb1c557a57fb514ac6faf0871
Sheena Gupta <aries.sheen@gmail.com>
2023-10-24 17:34:30 +0000

    Added MostPopularMovie method

--- code.js
+++ code.js
@@ -7,5 +7,41 @@ class MostPopularMovieSystem {
     addUser(userId, movies) {
       this.userMovies[userId] = new Set(movies);  //This method allows you to add a user and their list of favorite movies to the userMovies object. It takes userId and an array of movies.
     }
+
+    getMostPopularMovie(userId) {
+      if (!this.userMovies[userId]) {
+        return null;
+      }
+  
+      const userMovies = this.userMovies[userId];
+      const friendMovies = new Set();
+  
+      this.getFriends(userId).forEach((friend) => {
+        friendMovies.add(...(this.userMovies[friend] || []));
+      });
+  
+      const recommendedMovies = [...friendMovies].filter((movie) => !userMovies.has(movie));
+  
+      if (recommendedMovies.length === 0) {
+        return null;
+      }
+  
+      const movieCounts = {};
+  
+      this.getFriends(userId).forEach((friend) => {
+        recommendedMovies.forEach((movie) => {
+          if (this.userMovies[friend] && this.userMovies[friend].has(movie)) {
+            movieCounts[movie] = (movieCounts[movie] || 0) + 1;
+          }
+        });
+      });
+  
+      const mostPopularMovie = Object.keys(movieCounts).reduce((a, b) =>
+        movieCounts[a] > movieCounts[b] ? a : b
+      );
+  
+      return mostPopularMovie;
+    }
+  
   
 }
